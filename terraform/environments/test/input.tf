f5488696b71d8a6b9e6f02975d1b24267c12af18
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-06-09 17:06:00 +0100

    Update input.tf

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,16 +1,49 @@
 # Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  default = "b36c07e6-4e1b-48b7-9e22-ba384d37833a"
+}
+variable "client_id" {
+  default = "3c9068b9-c7b3-4518-9398-8bff15fa6500"
+}
+variable "client_secret" {
+  default = "7ae8Q~t6-weig4nRZJGnlUVsBPY.6ymckDCQMafZ"
+}
+variable "tenant_id" {
+  default = "d879c1a3-4270-4d76-a9b6-dcd67e3c53dc"
+}
 
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
-variable "application_type" {}
+
+variable "location" {
+  default = "eastus"
+}
+variable "resource_group" {
+  default = "ensuring-qa-release-rg"
+}
+variable "webapp_name" {
+  default = "qa-monitoring"
+}
+
+variable "application_type" {
+  default = "ev-eastus-001"
+}
 
 # Network
-variable virtual_network_name {}
-variable address_prefix_test {}
-variable address_space {}
+variable "virtual_network_name" {
+  default = "qa-virtual-net"
+}
+variable "address_prefix_test" {
+  default = ["10.0.2.0/24"]
+}
+variable "address_space" {
+  default = ["10.5.0.0/16"]
+}
 
+# Virtual Machine
+variable "vm_admin_username" {
+  default = "adedaryorh"
+}
+variable "vm_public_key" {}
+
+# Tags
+variable "project" {}
