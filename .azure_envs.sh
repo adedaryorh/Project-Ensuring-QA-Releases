62c2252571914a971bf04237e1754b8e97cff708
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-06-09 17:09:53 +0100

    Create .azure_envs.sh

---
+++ .azure_envs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Setting environment variables for Terraform"
+export ARM_SUBSCRIPTION_ID="b36c07e6-4e1b-48b7-9e22-ba384d37833a"
+export ARM_CLIENT_ID="3c9068b9-c7b3-4518-9398-8bff15fa6500"
+export ARM_CLIENT_SECRET="7ae8Q~t6-weig4nRZJGnlUVsBPY.6ymckDCQMafZ"
+export ARM_TENANT_ID="d879c1a3-4270-4d76-a9b6-dcd67e3c53dc"
+export ARM_ACCESS_KEY="00000000-0000-0000-0000-000000000000"
+echo "Done"
