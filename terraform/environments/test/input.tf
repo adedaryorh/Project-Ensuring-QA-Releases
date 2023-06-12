19ea08cfa42aaf905689fe754cd487606a9e4f35
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-06-12 14:29:37 +0100

    Update input.tf

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -3,21 +3,39 @@ variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
-
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
-variable "webapp_name" {}
+variable "location" {
+  default = "eastus"
+}
+variable "resource_group" {
+  default = "ensuring-qa-release-rg"
+}
+variable "webapp_name" {
+  default = "qa-monitoring"
+}
+
+variable "application_type" {
+  default = "ev-eastus-001"
+}
 
 # Network
-variable virtual_network_name {}
-variable "address_prefix_test" {}
-variable address_space {}
+variable "virtual_network_name" {
+  default = "qa-virtual-net"
+}
+variable "address_prefix_test" {
+  default = ["10.0.2.0/24"]
+}
+variable "address_space" {
+  default = ["10.5.0.0/16"]
+}
 
 # Virtual Machine
-variable vm_admin_username {}
-variable vm_public_key {}
+variable "vm_admin_username" {
+  default = "adedaryorh"
+}
+variable "vm_public_key" {}
 
 # Tags
 variable "project" {}
+
 
