98dd090b8300a43b4243b208b1aa247245456267
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-06-15 11:57:07 +0100

    Update input.tf

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,19 +1,42 @@
 # Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  default = "b36c07e6-4e1b-48b7-9e22-ba384d37833a"
+}
+variable "client_id" {
+  default = "6f44cc95-d3d9-4180-a2d1-84402c5cded4"
+}
+variable "client_secret" {
+  default = "cPW8Q~Qh0OkLY3zQLlWSmyhDLD2QUwdjcZAssbt_"
+}
+variable "tenant_id" {
+  default = "d879c1a3-4270-4d76-a9b6-dcd67e3c53dc"
+}
 
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
+variable "location" {
+  default = "eastus"
+}
+variable "resource_group" {
+  default = "ensuring-qa-release-rg"
+}
+variable "webapp_name" {
+  default = "qa-monitoring"
+}
+
 variable "application_type" {}
 
 # Network
-variable virtual_network_name {}
-variable address_prefix_test {}
-variable address_space {}
+variable "virtual_network_name" {}
+variable "address_prefix_test" {}
+variable "address_space" {}
 
+# Virtual Machine
+variable "vm_admin_username" {
+  default = "adedaryorh"
+}
+variable "vm_public_key" {}
 
+# Tags
+variable "project" {}
 
 
